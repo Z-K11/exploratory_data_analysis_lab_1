b51a6c40ae9359122b5a09194c469ed1d50b67dd
Z-K11 <zulqarnainbazidkhail@gmail.com>
2025-03-06 19:22:11 +0500

    downlaod and save the data set at destination folder

---
+++ main.py
@@ -0,0 +1,15 @@
+import pandas as pd 
+import numpy as np 
+import os 
+import requests
+url = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-ML0232EN-SkillsNetwork/asset/iris_data.csv'
+destination_folder='data'
+os.makedirs(destination_folder,exist_ok=True)
+file_path=os.path.join(destination_folder,'iris_data.csv')
+response = requests.get(url)
+if os.path.exists(file_path):
+    print(f'Data set already present at {file_path}')
+else:
+    with open(file_path,"wb") as file:
+        file.write(response.content)
+    print(f'Download complete file saved at path {file_path}')
